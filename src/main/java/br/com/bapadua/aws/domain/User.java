31e0215d1e61a5c06b140cc2bb47b5049193c1b7
Bruno A. Padua <bapadua@outlook.com>
2019-06-16 03:22:49 -0300

    Map entities

--- src/main/java/br/com/bapadua/aws/domain/User.java
+++ src/main/java/br/com/bapadua/aws/domain/User.java
@@ -3,6 +3,15 @@ package br.com.bapadua.aws.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 import br.com.bapadua.aws.domain.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,14 +22,28 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "TB_USER")
 public class User {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(length = 60, nullable = false)
 	private String name;
+	@Column(length = 60, nullable = false, unique = true)
 	private String email;
+	@Column(length = 60, nullable = false)
 	private String password;
+	
+	@Column(length = 20, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private Role role;
+	
+	@OneToMany(mappedBy = "user")
 	private List<Request> requests = new ArrayList<Request>();
+	
+	@OneToMany(mappedBy = "user")
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
 }
