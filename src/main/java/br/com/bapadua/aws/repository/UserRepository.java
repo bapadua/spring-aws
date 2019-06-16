74798f40ceba8edc83dc82e1ef562f8fd16100d3
Bruno A. Padua <bapadua@outlook.com>
2019-06-16 04:49:53 -0300

    User repository

---
+++ src/main/java/br/com/bapadua/aws/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package br.com.bapadua.aws.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.bapadua.aws.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	@Query("SELECT u FROM TB_USER u WHERE u.email = ?1 AND u.password = ?2")
+	public Optional<User> login(String email, String password);
+	
+
+}
