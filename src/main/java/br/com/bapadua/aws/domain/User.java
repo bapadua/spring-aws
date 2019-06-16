31bae5d6440363724efdda5075f0db75e9a25cbd
Bruno A. Padua <bapadua@outlook.com>
2019-06-16 02:35:57 -0300

    User role added

--- src/main/java/br/com/bapadua/aws/domain/User.java
+++ src/main/java/br/com/bapadua/aws/domain/User.java
@@ -3,6 +3,7 @@ package br.com.bapadua.aws.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.bapadua.aws.domain.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,6 +19,7 @@ public class User {
 	private String name;
 	private String email;
 	private String password;
+	private Role role;
 	private List<Request> requests = new ArrayList<Request>();
 	private List<RequestStage> stages = new ArrayList<RequestStage>();
 	
